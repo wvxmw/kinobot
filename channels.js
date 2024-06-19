04cfce382dc2b1b8fc5a40ddaa155ffce92afe76
wvxmw <sunsxt@yandex.ru>
2024-06-19 17:42:56 +0400

    Changed channels

--- channels.js
+++ channels.js
@@ -1,11 +1,4 @@
 const channels = [
-   {
-      title: "ТОП ФИЛЬМЫ 2024",
-      url: "testbotchannelsubscribe",
-   },
-   {
-      title: "ЛЮТЫЕ КРИПТОВИКИ",
-      url: "testbotchannelsubscribe2",
-   },
+   
 ];
 module.exports = channels;
